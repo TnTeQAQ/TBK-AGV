d313c88831b04e75e8a418588dd07fabce5b11dd
TnTeQAQ <tnteqaq@gmail.com>
2024-12-04 10:47:21 +0800

    0.1

--- Controller.py
+++ Controller.py
@@ -1,18 +1,18 @@
+import json
+
 import tbkpy._core as tbkpy
 from can_py import _can_py
 from WTJY901.chs.JY901S import jy901s
 import time
 import pickle
-import numpy as np
-import math
 import threading
-from tzcp.ros.sensor_pb2 import IMU
-from tzcp.ros.geometry_pb2 import Vector3
-from google.protobuf.json_format import MessageToDict
+
 
 class Controller:
     def __init__(self) -> None:
-        self.can_data = {}
+        self._can_data = {}
+        self._imu_data = {}
+        self.data = {}
         self.listen_thread = threading.Thread(target=self.listen, daemon=True)
         self.listen_thread.start()
 
@@ -20,22 +20,56 @@ class Controller:
         while True:
             self.get_all_data()
 
-    def get_can_data(self):
+    def pack_msg(self, data, msg_type="unknown", serialize_type="pickle"):
+        serialize_func = pickle.dumps
+        if serialize_type == "pickle":
+            serialize_func = pickle.dumps
+        elif serialize_type == "json":
+            serialize_func = json.dumps
+        else:
+            pass
+        return {
+            "msg_type": msg_type,
+            "serialize_type": serialize_type,
+            "data": serialize_func(data),
+        }
+
+    @property
+    def can_data(self):
         recv = _can_py.recv()
         if not recv:
             return
         id, data = recv
-        # 通过can传回的数据
-        self.can_data[id] = pickle.dumps(data)
+        self._can_data[id] = self.pack_msg(
+            data={"id": id, "data": data},
+            msg_type="can",
+            serialize_type="pickle"
+        )
+        return self._can_data
 
-    def get_imu_data(self):
-        self.imu_data = jy901s.get_data()
-        return self.imu_data
+    @property
+    def imu_data(self):
+        self._imu_data = self.pack_msg(
+            data=jy901s.get_data(),
+            msg_type="imu",
+            serialize_type="pickle"
+        )
+        return self._imu_data
 
     def get_all_data(self):
-        self.imu_data = self.get_imu_data()
-        self.agv_msg = {
-            "can": self.motor_msg,
-            "imu": self.imu_data,
-        }
-        return self.agv_msg
+        self.data = self.pack_msg(
+            data={"can_data": self._can_data, "imu_data": self._imu_data},
+            msg_type="all",
+            serialize_type="pickle"
+        )
+        return self.data
+
+
+if __name__ == '__main__':
+    ctr = Controller()
+    try:
+        while True:
+            print(ctr.get_all_data())
+            time.sleep(0.03)
+    finally:
+        del _can_py
